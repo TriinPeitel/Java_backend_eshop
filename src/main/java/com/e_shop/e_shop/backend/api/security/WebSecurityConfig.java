177840a2d2331542f14373bc35348e1e3547f020
triinpeitel <triinpeitel@gmail.com>
2024-10-16 15:59:44 +0300

    Implementing security for endpoints but getting a 403 Forbidden response in Postman. The problem is probably in WebSecurityConfig.java because the tutorial is using an old version of http.authorizeHttpRequests()         // Specific exclusions or rules.         .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()         // Everything else should be authenticated.         .anyRequest().authenticated();

--- src/main/java/com/e_shop/e_shop/backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/e_shop/e_shop/backend/api/security/WebSecurityConfig.java
@@ -1,18 +1,37 @@
 package com.e_shop.e_shop.backend.api.security;
 
+import com.auth0.jwt.JWT;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 public class WebSecurityConfig {
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        //http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.authorizeHttpRequests(authz -> authz
+                //.requestMatchers(new AntPathRequestMatcher("/product")).permitAll()
+                .requestMatchers( "/product", "/auth/register", "/auth/login").permitAll()
+                //.requestMatchers(HttpMethod.GET, "/product", "/auth/register", "/auth/login").permitAll()
+                //.requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                .anyRequest().authenticated());
         return http.build();
 
     }
 }
+
+
